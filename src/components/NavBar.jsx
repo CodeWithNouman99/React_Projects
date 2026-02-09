5fe8fd51d4ed7b455c6e414649917b17a99ebeea
Muhammad Nouman <mrnoumannomi7@gmail.com>
2026-02-09 10:21:18 +0500

    Paste App

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,6 +3,7 @@ import { NavLink } from "react-router-dom";
 const NavBar = () => {
   return (
     <div className="flex flex-row gap-4 space-content-evenly">
+      <h1>Paste App</h1>
       <NavLink to="/">Home</NavLink>
       <NavLink to="/pastes">Paste</NavLink>
     </div>
